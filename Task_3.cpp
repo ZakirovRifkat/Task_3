ff00cf3168bea7a4733ea724b66592c2dc6562cd
ZakirovRifkat <zakirov2323rifkat@gmail.com>
2022-09-28 13:55:55 +0300

    До меня дошло, что я делал всё неправильно. Переделываем.

--- Task_3.cpp
+++ Task_3.cpp
@@ -38,56 +38,6 @@ vector<vector<double>> multiplyMatrixMatrix(vector <vector <double>> A, vector <
 				AB[i][j] += A[i][k] * B[k][j];
 	return AB;
 }
-double lenght(vector<double> vector)
-{
-	double sum = 0;
-	for (int i = 0; i < vector.size(); i++)
-		sum += pow(vector[i], 2);
-	return sqrt(sum);
-}
-
-//vector<double> Gauss(vector<vector<double>> matrix_A, vector<double> vector_b)
-//{
-//	int p, n = matrix_A.size();
-//	double r, c, s;
-//	vector<double>x(n), b(n);
-//	vector<vector<double>>a(n, vector<double>(n));
-//	a = matrix_A;
-//	b = vector_b;
-//	for (int k = 0; k < n; k++)
-//	{
-//		p = k;
-//		for (int m = k + 1; m < n; m++)
-//			if (abs(a[p][k]) < abs(a[m][k])) //поиск максимального ведущего элемента
-//				p = m;
-//		for (int j = k; j < n; j++)
-//		{
-//			r = a[k][j];
-//			a[k][j] = a[p][j];   //перестановка строк
-//			a[p][j] = r;
-//		}
-//		r = b[k];
-//		b[k] = b[p];   //перестановка свободных членов
-//		b[p] = r;
-//		for (int m = k + 1; m < n; m++)
-//		{
-//			c = a[m][k] / a[k][k];
-//			b[m] = b[m] - c * b[k]; //приведение матрицы к верхнетреугольному виду
-//			for (int i = k; i < n; i++)
-//				a[m][i] = a[m][i] - c * a[k][i];
-//		}
-//	}
-//	x[n - 1] = b[n - 1] / a[n - 1][n - 1];
-//	for (int k = n - 1; k >= 0; k--)
-//	{
-//		s = 0;
-//		for (int i = k + 1; i < n; i++)				//обратный ход метода Гаусса
-//			s = s + a[k][i] * x[i];
-//		x[k] = (b[k] - s) / a[k][k];
-//	}
-//	return x;
-//}
-
 double scalar(vector<double> a, vector<double> b)
 {
 	double ab = 0;
@@ -95,40 +45,7 @@ double scalar(vector<double> a, vector<double> b)
 		ab += a[i] * b[i];
 	return ab;
 }
-double degree_method(vector<vector<double>> A, double exSol)
-{
-	int n = A.size();
-	double lambda = 0;
-	vector<double> x_0(n), x_k(n);
-	for (int i = 0; i < n; i++)
-		x_0[i] = 1;
-	/*while(fabs(lambda - exSol) > 0.001)*/
-	for(int i = 0; i<10; i++)
-	{
-		x_k = multiplyMatrixVector(A, x_0);
-		lambda = scalar(x_k, x_0) / scalar(x_0, x_0);
-		x_0 = x_k;
-	}
-	return lambda;
-}
-vector<double> own_vector(vector<vector<double>> matrix, double lambda)
-{
-	int n = matrix.size();
-	double l = 0;
-	vector<vector<double>> A(n, vector<double>(n));
-	vector<double> null(n), own(n);
-	for (int i = 0; i < n; i++)
-		null[i] = 0;
-	A = matrix;
-	for (int i = 0; i < n; i++)
-		A[i][i] = A[i][i] - lambda;
-	//own = Gauss(A,null); ???????????????????????????????????????
-	l = lenght(own);
-	if (l != 1)
-		for (int i = 0; i < n; i++)
-			own[i] = own[i] / l;
-	return own;
-}
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -150,16 +67,16 @@ int main()
 	cout << "Исходная матрица:\n";
 	show(matrix_A);
 
-	max = degree_method(matrix_A, exact_number);
-	my_vector_1 = own_vector(matrix_A, max);
-	cout << "Степенной метод макс.собст.число = " << max << "\n";
-	//	<< "Соответствующий ему собственный вектор ( ";
-	//for (int i = 0; i < n; i++)
-	//{
-	//	if (i != n-1)
-	//		cout << my_vector_1[i] << " ; ";
-	//	else
-	//		cout << my_vector_1[i] << " )\n";
-	//}
+	//max = degree_method(matrix_A, exact_number);
+	//my_vector_1 = own_vector(matrix_A, max);
+	//cout << "Степенной метод макс.собст.число = " << max << "\n";
+	////	<< "Соответствующий ему собственный вектор ( ";
+	////for (int i = 0; i < n; i++)
+	////{
+	////	if (i != n-1)
+	////		cout << my_vector_1[i] << " ; ";
+	////	else
+	////		cout << my_vector_1[i] << " )\n";
+	////}
 	return(0);
 }
