1181b58882d9c7a90e9b7193b4e1a0fc75065b57
ZakirovRifkat <zakirov2323rifkat@gmail.com>
2022-09-30 19:41:49 +0300

    Адекватная реализация всех методов

--- Task_3.cpp
+++ Task_3.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+#include <fstream>
 #include <Eigen/Dense>
 
 using Eigen::MatrixXd;
@@ -15,8 +15,8 @@ void show(vector<vector<double>> matrix)
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
-			cout << "\t" << matrix[i][j];
-		cout << "\n";
+			cout << "|" << matrix[i][j];
+		cout << "|\n";
 	}
 }
 vector<double> multiplyMatrixVector(vector<vector<double>>A, vector<double>b)
@@ -45,38 +45,145 @@ double scalar(vector<double> a, vector<double> b)
 		ab += a[i] * b[i];
 	return ab;
 }
+double aposteriori_estimate(vector<vector<double>> A, vector<double> Y, double lambda)
+{
+	int n = Y.size();
+	vector<double> ay(n);
+	ay = multiplyMatrixVector(A, Y);
+	for (int i = 0; i < n; i++)
+		Y[i] = lambda * Y[i];
+	for (int i = 0; i < n; i++)
+		ay[i] = ay[i] - Y[i];
+	return sqrt(scalar(ay,ay)) / sqrt(scalar(Y,Y));
+}
+vector<double> degree_method(vector<vector<double>> A, double e)
+{
+	int n = A.size(), step =0;
+	double estimate = 1, lambda=0;
+	vector<double> y_0(n), y_k(n), solution(n+2);
+	y_0[0] = 1;
+	for (int i = 1; i < n; i++)
+		y_0[i] = 1;
+	while (estimate > e)
+	{
+		y_k = multiplyMatrixVector(A, y_0);
+		lambda = y_k[0] / y_0[0];
+	/*	cout << "\nлямбда = " << lambda << "\n";
+		cout << "\nВектор y_k:\n";
+		for (int i = 0; i < n; i++)
+			cout << y_k[i]<<"\n";*/
+		y_0 = y_k;
+		estimate = aposteriori_estimate(A, y_k, lambda);
+		//cout << "estimate = " << estimate<<"\n";
+		step++;
+	}
+	solution[0] = step;
+	solution[1] = lambda;
+	for (int i = 2; i < n+2; i++)
+		solution[i] = y_k[i - 2];
+	return solution;
+}
+vector<double> scalar_method(vector<vector<double>> A, double e)
+{
+	int n = A.size(), step = 0;
+	double estimate = 1, lambda = 0;
+	vector<double> y_0(n), y_k(n), solution(n + 2);
+	y_0[0] = 1;
+	for (int i = 1; i < n; i++)
+		y_0[i] = 1;
+	while (estimate > e)
+	{
+		y_k = multiplyMatrixVector(A, y_0);
+		lambda = scalar(y_k, y_0) / scalar(y_0, y_0);
+		/*	cout << "\nлямбда = " << lambda << "\n";
+		cout << "\nВектор y_k:\n";
+		for (int i = 0; i < n; i++)
+			cout << y_k[i]<<"\n";*/
+		y_0 = y_k;
+		estimate = aposteriori_estimate(A, y_k, lambda);
+		//cout << "estimate = " << estimate<<"\n";
+		step++;
+	}
+	solution[0] = step;
+	solution[1] = lambda;
+	for (int i = 2; i < n + 2; i++)
+		solution[i] = y_k[i - 2];
+	return solution;
+}
+vector<double> reverse_scalar(vector<vector<double>> A, double lambda)
+{
+	int n = A.size();
+	double reverse_lambda = 0;
+	vector<vector<double>> B (n, vector<double>(n));
+	vector<double> vector(n+2);
+	for (int i = 0; i < n; i++)
+		B[i][i] = A[i][i] - lambda;
+	vector = scalar_method(B, 0.000001);
+	reverse_lambda = vector[1] + lambda;
+	vector[1] = reverse_lambda;
+	return vector;
+}
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	cout << "Задание 3. Проблема собственных значений. Вариант 4. Матрица - 8. \n";
+	cout << "Задание 3. Проблема собственных значений. Вариант 4. Матрица 8.\n";
 	int n;
-	double e = 0.000001, max = 0;
-	cout << "Введите размерность матрицы А:\n"
-		<<"n = ";
-	cin >> n;
+	double e = 0.000001, ex_max_value = 1.110;
+	ifstream file("\Text.txt");
+	file >> n;
+	cout << "Размерность матрицы n = " << n<<"\n";
 	vector<vector<double>> matrix_A(n, vector<double>(n));
-	vector<double> exSolution, my_vector_1(n);
+	vector<double> sol_1(n + 2), sol_2(n+2), sol_3(n+2),x(n);
 
-	double exact_number = 11;
-	cout << "Заполните вашу матрицу:\n";
+	//Заполняем матрицу
 	for(int i=0; i<n; i++)
 		for (int j = 0; j < n; j++)
-			cin >> matrix_A[i][j];
+			file >> matrix_A[i][j];
 
 	cout << "Исходная матрица:\n";
 	show(matrix_A);
 
-	//max = degree_method(matrix_A, exact_number);
-	//my_vector_1 = own_vector(matrix_A, max);
-	//cout << "Степенной метод макс.собст.число = " << max << "\n";
-	////	<< "Соответствующий ему собственный вектор ( ";
-	////for (int i = 0; i < n; i++)
-	////{
-	////	if (i != n-1)
-	////		cout << my_vector_1[i] << " ; ";
-	////	else
-	////		cout << my_vector_1[i] << " )\n";
-	////}
+	//степенной метод
+	sol_1 = degree_method(matrix_A, 0.001);
+	cout << "\nМаксимальное собственное число cтепенным методом = " << (sol_1[1]) << "\n"
+		<< "Соотвествующий ему собственный вектор: ( ";
+	for (int i = 2; i < n + 2; i++)
+	{
+		if (i != n + 1)
+			cout << sol_1[i] << "; ";
+		else
+			cout << sol_1[i] << " )";
+	}
+	cout << "; Кол-во итераций = " << sol_1[0]<<"\n";
+;
+	
+	//метод скалярных произведений
+	sol_2 = scalar_method(matrix_A, 0.000001);
+	cout << "\nМаксимальное собственное число методом скалярных произведений = " << (sol_2[1]) << "\n"
+		<< "Соотвествующий ему собственный вектор: ( ";
+	for (int i = 2; i < n + 2; i++)
+	{
+		if (i != n + 1)
+			cout << sol_2[i] << "; ";
+		else
+			cout << sol_2[i] << " )";
+	}
+	cout << "; Кол-во итераций = " << sol_2[0] << "\n";
+
+
+	//обратная граница:
+		sol_3 = reverse_scalar(matrix_A, sol_2[1]);
+	cout << "\nПротивоположная граница спектра = " << (sol_3[1]) << "\n"
+		<< "Соотвествующий ему собственный вектор: ( ";
+	for (int i = 2; i < n + 2; i++)
+	{
+		if (i != n + 1)
+			cout << sol_3[i] << "; ";
+		else
+			cout << sol_3[i] << " )";
+	}
+	cout << "; Кол-во итераций = " << sol_3[0] << "\n";
+
 	return(0);
 }
